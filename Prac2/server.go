dda71d73d29ec0a6b5216d63db3011c8a3cc097e
FHN1V2 <parfenov200407@gmail.com>
2023-11-08 10:32:53 +0700

    Ready

--- Prac2/server.go
+++ Prac2/server.go
@@ -10,7 +10,8 @@ import (
 type Database struct {
 	Stack *Stack
 	Queue *Queue
-	set *Set
+	set   *Set
+	hmap  *HashMap
 }
 
 func NewDatabase() *Database {
@@ -19,14 +20,14 @@ func NewDatabase() *Database {
 		Stack: &Stack{},
 		Queue: &Queue{},
 		set:   set,
+		hmap: &HashMap{},
 	}
 }
-
 
 func handleConnection(conn net.Conn, db *Database) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
-	//Set:=NewSet()
+
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
@@ -35,12 +36,20 @@ func handleConnection(conn net.Conn, db *Database) {
 		}
 
 		input := string(buf[:n])
-		fmt.Printf("Received input: %s\n")
+		fmt.Printf("Received input: %s\n",input)
 
 		parts := strings.Fields(input)
+
+
 		command := parts[0]
+
 		switch command {
+
 		case "SPUSH":
+			if len(parts) < 2 {
+				conn.Write([]byte("Command must have at least 2 arguments\n"))
+				continue
+			}
 			value := parts[1]
 			db.Stack.Spush(value)
 			conn.Write([]byte("Value pushed to stack: " + value + "\n"))
@@ -48,6 +57,10 @@ func handleConnection(conn net.Conn, db *Database) {
 			poppedValue := db.Stack.Spop()
 			conn.Write([]byte("Popped value from stack: " + poppedValue + "\n"))
 		case "QPUSH":
+			if len(parts) < 2 {
+				conn.Write([]byte("Command must have at least 2 arguments\n"))
+				continue
+			}
 			value := parts[1]
 			db.Queue.Qpush(value)
 			conn.Write([]byte("Value pushed to queue: " + value + "\n"))
@@ -55,13 +68,50 @@ func handleConnection(conn net.Conn, db *Database) {
 			poppedValue := db.Queue.Qpop()
 			conn.Write([]byte("Popped value from queue: " + poppedValue + "\n"))
 		case "SETPUSH":
-			value :=parts[1]
+			if len(parts) < 2 {
+				conn.Write([]byte("Command must have at least 2 arguments\n"))
+				continue
+			}
+			value := parts[1]
 			db.set.SetAdd(value)
 			conn.Write([]byte("Value pushed to set: " + value + "\n"))
 		case "SETPRINT":
 			setContents := db.set.SetPrint()
 			conn.Write([]byte(setContents + "\n"))
-		
+		case "SETDEL":
+			if len(parts) < 2 {
+				conn.Write([]byte("Command must have at least 2 arguments\n"))
+				continue
+			}
+			value := parts[1]
+			db.set.SetRemove(value)
+		case "HPUSH":
+			if len(parts) < 3 {
+				conn.Write([]byte("Command must have at least 3 arguments\n"))
+				continue
+			}
+			key, value := parts[1], parts[2]
+			db.hmap.Hadd(key, value)
+			conn.Write([]byte("Value pushed to hashtable: " + value + "\n"))
+		case "HDEL":
+			if len(parts) < 2 {
+				conn.Write([]byte("Command must have at least 2 arguments\n"))
+				continue
+			}
+			key := parts[1]
+			db.hmap.Hdel(key)
+		case "HGET":
+		if len(parts) < 2 {
+			conn.Write([]byte("Command must have at least 2 arguments\n"))
+			continue
+		}
+		key := parts[1]
+		poppedValue,err:= db.hmap.Hget(key)
+		if err != nil{
+			conn.Write([]byte("Key not found in hash table.\n"))
+			continue
+		}
+		conn.Write([]byte("Value for key "+key+" is "+poppedValue+".\n"))
 		default:
 			conn.Write([]byte("Unknown command: " + command + "\n"))
 		}
@@ -72,7 +122,7 @@ func handleConnection(conn net.Conn, db *Database) {
 }
 
 func main() {
-	listen, err := net.Listen("tcp", "0.0.0.0:6978")
+	listen, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Printf("Failed to bind to port: %s\n", err)
 		return
@@ -80,7 +130,7 @@ func main() {
 
 	defer listen.Close()
 
-	fmt.Println("Server is listening on port 6978")
+	fmt.Println("Server is listening on port 6379")
 
 	var wg sync.WaitGroup
 	db := NewDatabase()
@@ -98,4 +148,6 @@ func main() {
 			handleConnection(conn, db)
 		}()
 	}
+
+	
 }
